fe5f575d2b54e01e82703b0ad3ceb11bb2dfd1c5
Elisberto Junior <elisberto_jr@hotmail.com>
2023-05-03 18:09:18 -0300

    terminando exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+import PostComment from ".";
+
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar dois comentários corretamente", () => {
+    render(<PostComment />);
+
+    const commentTextarea = screen.getByTestId("comment-textarea");
+    const commentButton = screen.getByTestId("comment-button");
+
+    fireEvent.change(commentTextarea, {
+      target: { value: "Primeiro comentario" },
     });
+    fireEvent.click(commentButton);
+
+    fireEvent.change(commentTextarea, {
+      target: { value: "Segundo comentário" },
+    });
+    fireEvent.click(commentButton);
+
+    const commentElements = screen.getAllByTestId('comment');
+    expect(commentElements).toHaveLength(2);
+  });
 });
